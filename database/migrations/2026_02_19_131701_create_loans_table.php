f8c9383a5991b03b6e9176cdc27f3a15f6de1c14
rmorataya11 <renemorataya11@gmail.com>
2026-02-19 08:03:42 -0600

    migraciones para libros, prestamos y csv

--- database/migrations/2026_02_19_131701_create_loans_table.php
+++ database/migrations/2026_02_19_131701_create_loans_table.php
@@ -1,25 +1,23 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre_solicitante');
+            $table->timestamp('fecha_prestamo');
+            $table->timestamp('fecha_devolucion')->nullable();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('loans');
