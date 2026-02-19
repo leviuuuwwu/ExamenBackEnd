749ef2382846678873f13697a07baffb174f73b4
rmorataya11 <renemorataya11@gmail.com>
2026-02-19 08:30:18 -0600

    creacion de seeders (book y database)

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
 use Illuminate\Database\Seeder;
 
 class BookSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $path = database_path('data/books_classics.csv');
+        $file = fopen($path, 'r');
+        $headers = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            Book::create([
+                'title' => $row[0],
+                'description' => $row[1],
+                'isbn' => $row[2],
+                'total_copies' => (int) $row[3],
+                'available_copies' => (int) $row[4],
+                'status' => (bool) $row[5],
+            ]);
+        }
+
+        fclose($file);
+        Book::factory(90)->create();
     }
 }
