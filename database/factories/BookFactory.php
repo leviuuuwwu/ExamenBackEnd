1b72333a5d8915c12ef3e44588e79bc26b0db7f7
rmorataya11 <renemorataya11@gmail.com>
2026-02-19 08:37:07 -0600

    implementando bookfactory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,20 +4,20 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
- */
+
 class BookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $total = $this->faker->numberBetween(1, 10);
+        $available = $this->faker->numberBetween(0, $total);
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'isbn' => $this->faker->isbn13(),
+            'total_copies' => $total,
+            'available_copies' => $available,
+            'status' => $available > 0,
         ];
     }
 }
